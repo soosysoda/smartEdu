90613fc5c3ba4354fd3a15a7e2d4d5b82a265c38
Arkapravo Mukherjee <arkapravomukherjee7@gmail.com>
2025-03-26 22:07:00 +0530

    Add files via upload

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex justify-between items-center bg-blue-700 text-green-300 p-2 border-t-2 border-green-600'>
+        <p>© Copyright 2025 Team Syntax Error</p>
+        <p>Smart Bengal Hackathon Senior 2025</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/common/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Topbar from '../Layout/Topbar'
+import Navbar from '../Layout/Navbar'
+
+const Header = () => {
+  return (
+    <>
+    <Topbar />
+    <Navbar />
+    </>
+  )
+}
+
+export default Header
